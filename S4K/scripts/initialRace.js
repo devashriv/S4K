01199aa0d4e512b61cb22e5105d8b038cffb21ff
Dee Vagholkar <devashri.vagholkar@ansys.com>
2020-12-14 14:39:09 -0500

    ducky1 ply exporter added

--- S4K/scripts/initialRace.js
+++ S4K/scripts/initialRace.js
@@ -2,20 +2,42 @@
 
 import * as THREE from '../../build/three.module.js';
 
+import { OrbitControls } from '../../examples/jsm/controls/OrbitControls.js';
+
+import { PLYLoader } from "../../examples/jsm/loaders/PLYLoader.js";
 
 let camera, scene, renderer;
 
 let mesh, geometry, material, clock;
 
-const worldWidth = 128, worldDepth = 128;
+let objects, ducky1;
+
+const worldWidth = 500, worldDepth = 500;
 
 init();
 animate();
 
 function init() {
 
+    renderer = new THREE.WebGLRenderer( { antialias: true } );
+    renderer.setPixelRatio( window.devicePixelRatio );
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    document.body.appendChild( renderer.domElement );
+
     camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 1, 20000 );
     camera.position.y = 200;
+    camera.lookAt(0,0,-1);
+    //camera.position.set( 1, 2, - 3 );
+    //camera.lookAt( 0, 1, 0 );
+    
+
+    let controls = new OrbitControls( camera, renderer.domElement );
+				controls.addEventListener( 'change', render );
+				controls.minDistance = 50;
+				controls.maxDistance = 1000;
+				controls.enablePan = false;
+				controls.target.set( 0, 20, 0 );
+				controls.update();
 
     clock = new THREE.Clock();
 
@@ -45,12 +67,12 @@ function init() {
     mesh = new THREE.Mesh( geometry, material );
     scene.add( mesh );
 
-    renderer = new THREE.WebGLRenderer( { antialias: true } );
-    renderer.setPixelRatio( window.devicePixelRatio );
-    renderer.setSize( window.innerWidth, window.innerHeight );
-    document.body.appendChild( renderer.domElement );
+    
 
     window.addEventListener( 'resize', onWindowResize, false );
+    loadDucky1();
+
+
 
 }
 
@@ -94,3 +116,30 @@ function render() {
     renderer.render( scene, camera );
 
 }
+
+function loadDucky1() {
+    const texture = new THREE.TextureLoader().load( "./media/golfball.jpg" );
+    var loader = new PLYLoader();
+    loader.load(
+      "./models/Ducky1.ply",
+      function (geometry) {
+        geometry.computeVertexNormals();
+        var material = new THREE.MeshBasicMaterial( { map: texture } );
+        ducky1 = new THREE.Mesh(geometry, material);
+
+        //default position
+        ducky1.position.y = 50;
+        ducky1.position.x=-50;
+
+
+
+        ducky1.castShadow = true;
+        ducky1.receiveShadow = true;
+
+        scene.add(ducky1);
+        //objects.push(ducky1);
+
+
+      }
+    );
+  }
