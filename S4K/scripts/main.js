f3b92bb31b019e1ccb94081b96d121250bbc39d9
Dee Vagholkar <devashri.vagholkar@ansys.com>
2020-12-14 11:12:03 -0500

    code cleaned

---
+++ S4K/scripts/main.js
@@ -0,0 +1,6 @@
+//all imported scripts go here
+
+import * as THREE from '../build/three.module.js';
+
+import { GUI } from './jsm/libs/dat.gui.module.js';
+import { OrbitControls } from './jsm/controls/OrbitControls.js';
