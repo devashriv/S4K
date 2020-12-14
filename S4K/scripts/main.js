e3e8f387f1ff5d5a84c8e53fc7aa1881d17ff525
Dee Vagholkar <devashri.vagholkar@ansys.com>
2020-12-14 11:49:50 -0500

    wavy water background added

--- S4K/scripts/main.js
+++
@@ -1,6 +0,0 @@
-//all imported scripts go here
-
-import * as THREE from '../build/three.module.js';
-
-import { GUI } from './jsm/libs/dat.gui.module.js';
-import { OrbitControls } from './jsm/controls/OrbitControls.js';
