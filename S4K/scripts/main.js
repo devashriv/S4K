b5510c0e8b30cb4227aa5ec58ace3610911b0397
Colton Ord <coor9349@colorado.edu>
2020-12-14 22:18:51 -0500

    Adding checks

--- S4K/scripts/main.js
+++ S4K/scripts/main.js
@@ -1,12 +1,149 @@
-function ChangeImage2() {
-  document.getElementById("ClickImage").disabled = true;
-  var conf = confirm("Are you sure you want this duck?");
+function ChangeImage11() {
+  //document.getElementById("ClickImage").disabled = true;
+  var conf = confirm("Are you sure you want this duck, Pink Plastic?");
   if (conf == true)
+  {
+    document.getElementById("P1P").src = "../S4K/media/check.png";
+    document.getElementById("P1P").alt = "check"
+  }
+  if (checkhowmany() == 3)
   {
     document.getElementById("buttonpage").style.visibility = 'visible';
   }
-  else
+}
+
+function ChangeImage12() {
+  //document.getElementById("ClickImage").disabled = true;
+  var conf = confirm("Are you sure you want this duck, 12?");
+  if (conf == true)
   {
-    window.location.href = "screen2.html";
+    document.getElementById("R1Y").src = "../S4K/media/check.png";
+    document.getElementById("R1Y").alt = "check"
   }
+  if (checkhowmany() == 3)
+  {
+    document.getElementById("buttonpage").style.visibility = 'visible';
+  }
+}
+
+function ChangeImage13() {
+  //document.getElementById("ClickImage").disabled = true;
+  var conf = confirm("Are you sure you want this duck, 13?");
+  if (conf == true)
+  {
+    document.getElementById("W1B").src = "../S4K/media/check.png";
+    document.getElementById("W1B").alt = "check"
+  }
+  if (checkhowmany() == 3)
+  {
+    document.getElementById("buttonpage").style.visibility = 'visible';
+  }
+}
+
+function ChangeImage21() {
+  //document.getElementById("ClickImage").disabled = true;
+  var conf = confirm("Are you sure you want this duck, 21?");
+  if (conf == true)
+  {
+    document.getElementById("P2P").src = "../S4K/media/check.png";
+    document.getElementById("P2P").alt = "check"
+  }
+  if (checkhowmany() == 3)
+  {
+    document.getElementById("buttonpage").style.visibility = 'visible';
+  }
+}
+
+function ChangeImage22() {
+  //document.getElementById("ClickImage").disabled = true;
+  var conf = confirm("Are you sure you want this duck, 22?");
+  if (conf == true)
+  {
+    document.getElementById("R2Y").src = "../S4K/media/check.png";
+    document.getElementById("R2Y").alt = "check"
+  }
+  if (checkhowmany() == 3)
+  {
+    document.getElementById("buttonpage").style.visibility = 'visible';
+  }
+}
+
+function ChangeImage23() {
+  //document.getElementById("ClickImage").disabled = true;
+  var conf = confirm("Are you sure you want this duck, 23?");
+  if (conf == true)
+  {
+    document.getElementById("W2B").src = "../S4K/media/check.png";
+    document.getElementById("W2B").alt = "check"
+  }
+  if (checkhowmany() == 3)
+  {
+    document.getElementById("buttonpage").style.visibility = 'visible';
+  }
+}
+
+function ChangeImage31() {
+  //document.getElementById("ClickImage").disabled = true;
+  var conf = confirm("Are you sure you want this duck, 31?");
+  if (conf == true)
+  {
+    document.getElementById("P3P").src = "../S4K/media/check.png";
+    document.getElementById("P3P").alt = "check"
+  }
+  if (checkhowmany() == 3)
+  {
+    document.getElementById("buttonpage").style.visibility = 'visible';
+  }
+}
+
+function ChangeImage32() {
+  //document.getElementById("ClickImage").disabled = true;
+  var conf = confirm("Are you sure you want this duck, 32?");
+  if (conf == true)
+  {
+    document.getElementById("R3Y").src = "../S4K/media/check.png";
+    document.getElementById("R3Y").alt = "check"
+  }
+  if (checkhowmany() == 3)
+  {
+    document.getElementById("buttonpage").style.visibility = 'visible';
+  }
+}
+
+function ChangeImage33() {
+  //document.getElementById("ClickImage").disabled = true;
+  var conf = confirm("Are you sure you want this duck, 33?");
+  if (conf == true)
+  {
+    document.getElementById("W3B").src = "../S4K/media/check.png";
+    document.getElementById("W3B").alt = "check"
+  }
+  if (checkhowmany() == 3)
+  {
+    document.getElementById("buttonpage").style.visibility = 'visible';
+  }
+}
+
+function checkhowmany()
+{
+  var counter = 0;
+  if (document.getElementById("P1P").alt == "check") {counter++}
+
+  if (document.getElementById("P2P").alt == "check") {counter++}
+
+  if (document.getElementById("P3P").alt == "check") {counter++}
+
+  if (document.getElementById("R1Y").alt == "check") {counter++}
+
+  if (document.getElementById("R2Y").alt == "check") {counter++}
+
+  if (document.getElementById("R3Y").alt == "check") {counter++}
+
+  if (document.getElementById("W1B").alt == "check") {counter++}
+
+  if (document.getElementById("W2B").alt == "check") {counter++}
+
+  if (document.getElementById("W3B").alt == "check") {counter++}
+
+  return (counter)
 }
