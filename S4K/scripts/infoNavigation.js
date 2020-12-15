2104967c7c44530a614288eca798b4652e197069
Dee Vagholkar <devashri.vagholkar@ansys.com>
2020-12-14 23:45:01 -0500

    screen1 information and video added

--- S4K/scripts/infoNavigation.js
+++ S4K/scripts/infoNavigation.js
@@ -1,20 +1,29 @@
-let materials=["wood", "rubber", "plastic"];
-let shapes=["Shape1", "Shape2", "Shape3"];
-
+let materials=["Wooden", "Rubber", "Plastic"];
+let shapes=["Short neck", "Open wing", "Long neck"];
+let shapeInformation = [
+    "a duck with this shape offers more air and water resistance than one of the other options due to its large frontal area",
+    "a duck with this shape offers the most air and water resistance as it has the largest frontal area",
+    "a duck with this shape offers least air and water resistance as it has the least frontal area (most aerodynamically efficient shape)"
+]
+let matlInformation = [
+    "this is the least dense of the materials and thus for the same volume of the duck, building it out of wood will make it lighter than the other options",
+    "this is the densest material here and thus a duck made of rubber will be the heaviest option",
+    "this is the second least dense of the materials and building a duck of plastic will make it heavier than wood but lighter than rubber"
+]
+let videoPath=[
+    "../S4K/media/Duck-1.mp4",
+    "../S4K/media/Duck-3.mp4",
+    "../S4K/media/Duck-2.mp4"
+]
 function showInfo(matl, shape)
 {
-    document.getElementById("ClickImage").disabled = true;
-    var conf = confirm("Are you sure you want this duck?");
-    if (conf == true)
-    {
-      document.getElementById("buttonpage").style.visibility = 'visible';
-    }
-    else
-    {
-      window.location.href = "screen2.html";
-    }
-
-    
+    document.getElementById("itemTitle").innerHTML= materials[matl]+" "+shapes[shape];
+    document.getElementById("shapeInfo").innerHTML= shapeInformation[shape];
+    document.getElementById("matlInfo").innerHTML= matlInformation[matl];
+    document.getElementById("genInfo").innerHTML="All the ducks start the race with the same amount of energy imparted to them by the “catapult”. Thus, the lightest duck will start with the largest initial velocity. Assuming no losses, this means elastic energy of the catapult gets converted to kinetic energy for the duck";
+    document.getElementById("eqtn").style.visibility = "visible";
+    document.getElementById("simVid").src= videoPath[shape];
+    document.getElementById("simVid").style.visibility = "visible";
 }
 
 function getInfo(matl,shape)
